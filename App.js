89947315e3c0ed2525641dd740b5e1aeba004457
borragopinadh <123790349+borragopinadh@users.noreply.github.com>
2023-11-05 18:35:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,49 @@
+//  import DigitalClock1 from "./DigitalClock1";
+// import Example11 from "./Example11";
+// import Counter from "./Counter";
+// import ChangeColor from "./ChangeColor";.
+// import TableForm from "./TabularForm";
+// import Example7 from "./Example7";
+// import ArrayClass from "./ArrayClass";
+// import UseStateDemo from "./UseStateDemo";
+// import Example9 from "./Example9";
+// import Example10 from './Example10'
+import Bm1 from "./pratice/Bm1";
+// import DataFetching from "./DataFetching";
+// import DataFetching from "./DataFetching";
+//  import React from "react";
+//  import BlogContextDemo from "./BlogContextDemo";
+// // // import ArrayClass from "./ArrayClass";
+// const blogInfo={
+//   React:{
+//     post:"Learn useContext Hooks",
+//     author:"gopinadh"
+//   },
+//   NodeJS:{
+//     post:"Node Commands",
+//     author:"B ARJUN"
+//   }
+// };
+// export const BlogContext=React.createContext(blogInfo);
+function App(){
+  return(
+    
+    // <DigitalClock1/>
+    //  < ArrayClass/>
+    // <Counter/>
+     <Bm1/>
+    // <DataFetching/>
+  //   <BlogContext.Provider value={blogInfo}>
+  //   <BlogContextDemo/>
+  // </BlogContext.Provider>
+    // <ChangeColor/>
+    // <Example7/>
+    // <TableForm/>
+  // <UseStateDemo/>
+    // <Example9/>
+    // <Example11/>
+    // <Example10/>
+
+  )
+}
+export default App;
